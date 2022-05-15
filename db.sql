e2834666a993bbfc2e400036b78e6c0b850b4fb2
Varmilo <90532063+VarmiloVA@users.noreply.github.com>
2022-05-15 10:45:16 +0200

    .gitignore added

--- db.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE users(id Serial, name text, email text, age int);
-
-INSERT INTO users(name, email, age) VALUES
-    ('Daniel', 'varmilo.blue@protonmail.com', 15);
-
-select * from users;	
